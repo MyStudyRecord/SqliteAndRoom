59c568eee04818aadf65cfede0386c7f1d8be294
ParkSangSun <appleasz1028102@gmail.com>
2021-10-15 10:53:59 +0900

    setup

--- app/src/main/java/com/kdn/roomdemo/MainActivity.kt
+++ app/src/main/java/com/kdn/roomdemo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kdn.roomdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
